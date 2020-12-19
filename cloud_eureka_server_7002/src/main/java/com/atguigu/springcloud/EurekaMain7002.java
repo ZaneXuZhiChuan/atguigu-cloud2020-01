b0d5abfbf87494f7d544f7fc8fd2bac723909097
xuwu <x18907084487@gmail.com>
2020-12-19 15:39:49 +0800

    目前是http方式提交代码到github，所以每次提交代码到远端时候都要输入好几次代码；所以现在要将http提交方式转换成ssh方式；

--- cloud_eureka_server_7002/src/main/java/com/atguigu/springcloud/EurekaMain7002.java
+++ cloud_eureka_server_7002/src/main/java/com/atguigu/springcloud/EurekaMain7002.java
@@ -11,6 +11,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaMain7002 {
     public static void main(String[] args) {
+        /*
+        目前是http方式提交代码到github，所以每次提交代码到远端时候都要输入好几次代码；所以现在要将http提交方式转换成ssh方式；
+         */
         SpringApplication.run(EurekaMain7002.class, args);
     }
 }
