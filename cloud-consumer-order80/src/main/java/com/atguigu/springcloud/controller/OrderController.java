9f603c7d99dd801e63d406024fcc8d3c7831f338
xuwu <x18907084487@gmail.com>
2020-12-19 19:20:27 +0800

    集成Feign服务调用模块并测试通过

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,5 +28,19 @@ public class OrderController {
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/"+id,CommonResult.class);
+    }
+
+    /*
+    RestTemplate的 getForEntity() 和 getForEntity() 两个函数的区别在于 getForEntity() 返回的是Json字符串，
+    而 getForEntity() 返回的是 ResponseEntity 对象，相比较于 json 字符串，ResponseEntity包含了响应头、响应状态码、响应体等内容；
+     */
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPaymentById02(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else {
+            return new CommonResult<>(404,"操作错误");
+        }
     }
 }
