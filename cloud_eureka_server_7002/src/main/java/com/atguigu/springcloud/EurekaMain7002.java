cba352bb330442057a19bbe9cd973985b6471fcb
xuwu <x18907084487@gmail.com>
2020-12-19 15:29:16 +0800

    测试将git提交方式从http转换为ssh01

--- cloud_eureka_server_7002/src/main/java/com/atguigu/springcloud/EurekaMain7002.java
+++ cloud_eureka_server_7002/src/main/java/com/atguigu/springcloud/EurekaMain7002.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaMain7002 {
     public static void main(String[] args) {
+        System.out.println("测试一下。。。。");
         SpringApplication.run(EurekaMain7002.class, args);
     }
 }
