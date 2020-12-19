a3086480650c353dc45ac595c0e333a75cc69102
xuwu <x18907084487@gmail.com>
2020-12-19 15:18:56 +0800

    集成consul模块，有些小bug需要之后解决

---
+++ cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/controller/OrderConsulController.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class OrderConsulController {
+    public static final String INVOKE_URL = "http://cloud-payment-service";
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/consul")
+    public String paymentInfo(){
+        String result = restTemplate.getForObject(INVOKE_URL + "/payment/consul", String.class);
+        return result;
+    }
+}
