ab044f4c49e1326e0862ece8ef5ce774e05c9af0
xuwu <x18907084487@gmail.com>
2020-12-21 06:33:20 +0800

    测试了一个工具类：获取当前时区时间  获取的值对于配置spring gateway中的predicate参数有用

---
+++ cloud_gateway_gateway9527/src/test/java/T_01.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.ZonedDateTime;
+
+public class T_01 {
+    @Test
+    public void test01(){
+        System.out.println(ZonedDateTime.now());
+    }
+
+}
