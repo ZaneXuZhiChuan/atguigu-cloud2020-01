8bf2fbed29daf4c50afcf255b6ca00084dfd33c4
xuwu <x18907084487@gmail.com>
2020-12-22 16:30:47 +0800

    完成spring config手动动态刷新获取配置功能

--- cloud_config_client_3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud_config_client_3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -1,10 +1,14 @@
 package com.atguigu.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+该注解让 config client 端具有刷新能力；
+ */
+@RefreshScope
 @RestController
 public class ConfigClientController {
 
