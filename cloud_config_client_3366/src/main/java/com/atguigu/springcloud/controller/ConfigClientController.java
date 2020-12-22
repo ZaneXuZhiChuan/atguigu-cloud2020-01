bc13bae19559fe1d57f75bc34b751b922065f562
xuwu <x18907084487@gmail.com>
2020-12-22 21:04:19 +0800

    集成测试完成spring bus aqmp 功能：成功实现修改一处配置文件，即config server 3344配置服务应用处的配置文件，然后通过广播使得微服务中其它通过rabbitmq订阅了config server服务的配置文件也得以刷新

---
+++ cloud_config_client_3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+该注解让 config client 端具有刷新能力；
+ */
+@RefreshScope
+@RestController
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    String serverPort;
+
+    @Value("${config.info}")
+    String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return "configInfo: "+configInfo + "------serverPort: " + serverPort;
+    }
+
+}
