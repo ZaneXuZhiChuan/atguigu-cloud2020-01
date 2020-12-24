abaa547e2d72ea8b56e3d1463a1d0aa9441aff8f
xuwu <x18907084487@gmail.com>
2020-12-25 07:46:42 +0800

    更改9001、9002、83服务使它们环境能满足测试Sentinel的 fallback+blockHandler 功能：并且最终测试成功=====当blockHandler和fallback同时发生的时候如果先触发限流的规则，则blockHandler的逻辑优先

--- cloud_alibaba_consumer_nacos_order83/src/main/java/com/atguigu/springcloud/controller/OrderNacosController.java
+++ cloud_alibaba_consumer_nacos_order83/src/main/java/com/atguigu/springcloud/controller/OrderNacosController.java
@@ -1,11 +1,14 @@
 package com.atguigu.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.sertinel.fallback.MyFallBack;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -19,5 +22,27 @@ public class OrderNacosController {
     @GetMapping("/consumer/payment/nacos/{id}")
     public String paymentInfo(@PathVariable("id")Integer id){
         return restTemplate.getForObject(SEVER_URL + "/payment/nacos/" + id, String.class);
+    }
+
+    //  测试 Sentinel 的服务熔断功能
+    @RequestMapping("/consumer/fallback/{id}")
+    @SentinelResource(value = "fallback", fallback = "fallbackHandler",blockHandler = "blockHandler"/*,fallbackClass = MyFallBack.class*/)
+    public CommonResult<Payment> fallback(@PathVariable("id") Long id) {
+        CommonResult result = restTemplate.getForObject(SEVER_URL + "/payment/get/" + id, CommonResult.class, id);
+        if (id == 4) {
+            log.info("2".repeat(15) + "=========4");
+            throw new IllegalArgumentException("IllegalArgumentException ，非法参数异常。。。。");
+        } else if (result.getT() == null) {
+            log.info("2".repeat(15) + "=========NULL");
+            throw new NullPointerException("NullPointerException , 该ID没有值 空指针异常");
+        }
+        return result;
+    }
+    public CommonResult fallbackHandler(@PathVariable Long id, Throwable throwable) {
+        return new CommonResult<>(401,"fallbackException===Sentinel的fallback处理运行时异常： fallbackHandler " + "======="+ throwable.getMessage()+"====="+id);
+    }
+
+    public CommonResult blockHandler(@PathVariable Long id, BlockException exception) {
+        return new CommonResult<>(402,"Sentinel blockHandler 限流： blockHandler " + "======="+ exception.getMessage()+"====="+id);
     }
 }
--- cloud_alibaba_provider_payment9002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud_alibaba_provider_payment9002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -1,11 +1,19 @@
 package com.atguigu.springcloud.controller;
 
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
 
 @RestController
+@Slf4j
 public class PaymentController {
     @Value("${server.port}")
     String serverPort;
@@ -14,4 +22,42 @@ public class PaymentController {
     public String getPayment(@PathVariable("id")Integer id){
         return "Nacos registry, serverport: " + serverPort + "--------ID:" + id;
     }
+
+
+//  ====================================  以下代码是为了测试 Sentinel服务熔断 功能准备的测试环境
+    @Resource
+    PaymentService paymentService;
+    //    展示给调用者的信息
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @PostMapping("/payment/create")
+    public CommonResult create(@RequestBody Payment payment) {
+        int res = paymentService.create(payment);
+        log.info("********CREATE..." + serverPort + ": " + res);
+        if (res > 0){
+            return new CommonResult(200,"CREATE sucess..." + serverPort,res);
+        }else {
+            return new CommonResult(404,"CREATE filed..." + serverPort);
+        }
+    }
+
+    @GetMapping("/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id) {
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("=".repeat(5)+"********QUERY..." + serverPort + ": "  + payment);
+        if (payment != null){
+            return new CommonResult(200,"QUERY sucess..." + serverPort,payment);
+        }else {
+            return new CommonResult(404,"QUERY filed..." + serverPort);
+        }
+    }
+
+    //    通过 DiscoveryClient 可以获取注册进EurekaServer应用中的各种微服务的详细信息，包括该微服务的名称、IP、端口以及访问Uri等信息
+    @GetMapping("/payment/discovery")
+    public Object discovery(){
+        log.info("~".repeat(15) +"DiscoveryClient"+ serverPort + "-".repeat(5)+ discoveryClient.toString());
+        return this.discoveryClient;
+    }
+
 }
